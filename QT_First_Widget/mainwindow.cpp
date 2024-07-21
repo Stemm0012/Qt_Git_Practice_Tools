133cb9e035286aff6b3bba90c1ec207ddf9c8440
derpydurpy <SAS2024JS@yahoo.com>
2024-07-20 19:50:14 -0700

    click button 4 -> hide/show label
    completed

--- QT_First_Widget/mainwindow.cpp
+++ QT_First_Widget/mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+int test = 0;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     ui->label_2->hide();
+    ui->label_3->hide();
+
+
 }
 
 MainWindow::~MainWindow()
@@ -14,3 +19,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_4_clicked()
+{
+
+    if(test == 0)
+    {
+        ui->label_3->show();
+        test = 1;
+    }
+    else
+    {
+        ui->label_3->hide();
+        test = 0;
+    }
+}
