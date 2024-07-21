1fcf8e9ac221cfd47bb299cf11a3a81b501281a4
derpydurpy <SAS2024JS@yahoo.com>
2024-07-20 21:22:52 -0700

    Completed

--- QMessageBox_Testing/mainwindow.cpp
+++ QMessageBox_Testing/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -35,3 +36,20 @@ void MainWindow::on_pushButton_4_clicked()
     QMessageBox::information(this, "Information", "Helpful Tip #1: Close the Box");
 }
 
+void MainWindow::on_pushButton_5_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this, "Question", "Do you want to exit the app?", QMessageBox::Yes | QMessageBox::No);
+    if(reply == QMessageBox::Yes)
+    {
+        QApplication::quit();
+    }
+    else
+    {
+        qDebug() << "No is Clicked.";
+    }
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    QMessageBox::warning(this, "Warning!", "Don't click again!");
+}
